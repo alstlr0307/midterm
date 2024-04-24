69c999aaa3db989581a5ef7accacefb94cb63119
alstlr0307 <alstlr0307@naver.com>
2024-04-24 10:44:00 +0900

    커밋

--- src/Midterm.java
+++ src/Midterm.java
@@ -1,4 +1,9 @@
 package src;
+/*
+ * 이 프로그램은 StringBuffer 클래스를 이용하여 사용자로부터 영문(스페이스 포함)을 입력받고,
+ * 글자 하나만 랜덤하게 선택해서 다른 글자로 수정하는 프로그램입니다.
+ * 프로그램은 무한 반복되다가 exit라는 문자열이 입력되면 종료 됩니다.
+ */
 import java.util.Scanner; // (문제1) 다음 코드를 실행하기 위한 Scanner class를 import해 주세요.
 
 public class Midterm {
